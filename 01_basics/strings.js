b3ee74a6f3380b0f596d97dbfff00a15d0720e5c
Suryansh raj <145867110+rashmisuryanshraj@users.noreply.github.com>
2023-10-12 10:20:04 +0000

    stingsGame

---
+++ 01_basics/strings.js
@@ -0,0 +1,12 @@
+const name = "suryansh";
+console.log(name)
+
+// const gameName = "rashmi"
+// console.log(gameName)
+
+const gameName = new String('suryansh')
+console.log(gameName[0])
+console.log(gameName.__proto__)
+console.log(gameName.charAt());
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
