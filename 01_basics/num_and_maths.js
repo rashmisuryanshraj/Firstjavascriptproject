e76146b80fb2b148b8367b703c2b9449f8e3dc7f
Suryansh raj <145867110+rashmisuryanshraj@users.noreply.github.com>
2023-10-13 09:43:36 +0000

    num and maths

---
+++ 01_basics/num_and_maths.js
@@ -0,0 +1,8 @@
+const number = new Number(100)
+console.log(number);
+console.log(number.toString());
+console.log(number.toString().length);
+console.log(number.toFixed());
+
+const otherNumber = 845455.845455
+console.log(otherNumber.toPrecision(7));
