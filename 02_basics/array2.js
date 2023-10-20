5f5369a1a52cd1356477029c18b92b72cf87e41c
Suryansh raj <145867110+rashmisuryanshraj@users.noreply.github.com>
2023-10-20 02:43:22 +0000

    array2

--- 02_basics/array2.js
+++ 02_basics/array2.js
@@ -1,7 +1,31 @@
 const marvel_herros = ["thor","iron-man","caption-america"]
 const dc_heros = ["saktiman","krish","batman"]
+const num = [1,2,3,4,5]
 
-marvel_herros.push(dc_heros);
-console.log(marvel_herros);
-console.log(marvel_herros[3][2]);
+// marvel_herros.push(dc_heros);//push oparator
+// console.log(marvel_herros);
+// console.log(marvel_herros[3][2]);
+
+// const all_new_heros = marvel_herros.concat(dc_heros,num)//concat oparator
+// console.log(all_new_heros);
+
+// const all_heros =  [...marvel_herros,...dc_heros,...num]// shpeared oparator
+// console.log(all_heros);
+
+//flat oprator
+// const new_onther_array = [1,2,3,4,[1,2,3],5,6,7,[4,5,6,[2,4,6]]]
+
+// const real_unther_array = new_onther_array.flat(Infinity)
+// console.log(real_unther_array)
+
+// console.log(Array.isArray("Suryansh"))
+// console.log(Array.from("Suryansh"))
+// console.log(Array.from({name: "Suryansh"}))//intresting 
+
+const score1 = 100
+const score2= 200
+const score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
 
