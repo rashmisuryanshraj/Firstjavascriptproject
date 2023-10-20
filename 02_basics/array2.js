c614a89a310bf21d97a4d0dc99466732c77a1e3f
Suryansh raj <145867110+rashmisuryanshraj@users.noreply.github.com>
2023-10-20 02:03:13 +0000

    array2

---
+++ 02_basics/array2.js
@@ -0,0 +1,7 @@
+const marvel_herros = ["thor","iron-man","caption-america"]
+const dc_heros = ["saktiman","krish","batman"]
+
+marvel_herros.push(dc_heros);
+console.log(marvel_herros);
+console.log(marvel_herros[3][2]);
+
